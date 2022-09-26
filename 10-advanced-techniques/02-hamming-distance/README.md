e4ae2d52e343b6a9539b6ca1f674cfff9921c9e2
Lucca Miranda Nunes <ra230554@melo.lab.ic.unicamp.br>
2022-09-26 16:00:33 -0300

    add hamming distance 2136

---
+++ 10-advanced-techniques/02-hamming-distance/README.md
@@ -0,0 +1,3 @@
+# Hamming Distance
+
+[https://cses.fi/problemset/task/2136/](https://cses.fi/problemset/task/2136/)
---
+++ 10-advanced-techniques/02-hamming-distance/lucca.cpp
@@ -0,0 +1,43 @@
+/**
+* Sacada: hamming distance entre duas strings binárias
+* é igual ao popcount do xor.
+* O(n²) para encontrar o melhor par.
+**/
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long int ll;
+
+const int MAXN = 1e5 + 10, INF = 2e9, M = 1e9 + 7;
+
+int conv(string s) {
+	int res = 0;
+	for (int i = 0; i < s.size(); ++i)
+		if (s[i] == '1')
+			res |= (1 << i);
+	return res;
+}
+
+void solve() {
+	int n, k; cin >> n >> k;
+	vector<int> v(n);
+	for (int i = 0; i < n; ++i) {
+		string s; cin >> s; reverse(all(s));
+		v[i] = conv(s);
+	}
+	int ans = 30;
+	for (int i = 0; i < n; ++i)
+		for (int j = i + 1; j < n; ++j)
+			ans = min(ans, __builtin_popcount(v[i]^v[j]));
+	cout << ans << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	solve();
+	return 0;
+}
